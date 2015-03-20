f03ba6b880fcde7231ebbac8f3eee8331e1a9940
Jaroslav Psenicka <jaroslav.psenicka@gmail.com>
2015-03-20 23:39:34 +0000

    jmx completed

--- monitor/src/main/webapp/index.js
+++ monitor/src/main/webapp/index.js
@@ -133,5 +133,129 @@ app.controller('StatsCtrl', function($scope, $interval, $q, stats, tput) {
     };
 
     $scope.refresh();
+    setInterval(function() {
+        $scope.refresh();
+    }, 900000);
+
+});
+
+app.controller('JmxCtrl', function($scope, $interval, $q, stats, tput) {
+
+    $scope.jmx = new Jolokia("jmx");
+    $scope.req = [
+        { type: "read", mbean: "java.lang:type=OperatingSystem", attribute: "SystemCpuLoad"},
+        { type: "read", mbean: "kafka.server:type=BrokerTopicMetrics", attribute: "BytesInPerSec"},
+        { type: "read", mbean: "kafka.server:type=BrokerTopicMetrics", attribute: "BytesOutPerSec"}
+    ];
+
+    $scope.loadChart = {
+        title: { text: '' },
+        loading: false,
+        options: {
+            chart: {
+                type: 'area',
+                width: 200,
+                height: 100,
+                backgroundColor: 'transparent'
+            },
+            legend: { enabled: false },
+            plotOptions: {
+                area: {
+                    lineWidth: 1,
+                    lineColor: '#57D9F6',
+                    fillOpacity: '0.5',
+                    dataLabels: { enabled: false },
+                    marker: { enabled: false }
+                }
+            },
+            xAxis: {
+                type: 'datetime',
+                labels: {
+                    style: { color: 'lightgray' }
+                }
+            },
+            yAxis: {
+                gridLineColor: 'gray',
+                min: 0,
+                max: 100,
+                label: { enabled: false },
+                labels: {
+                    style: { color: 'lightgray' }
+                },
+                title: { enabled: false }
+            }
+        },
+        series: [{
+            name: 'System load',
+            data: []
+        }]
+    };
+
+    $scope.topicChart = {
+        title: { text: '' },
+        loading: false,
+        options: {
+            chart: {
+                type: 'area',
+                width: 500,
+                height: 100,
+                backgroundColor: 'transparent'
+            },
+            legend: { enabled: false },
+            plotOptions: {
+                area: {
+                    lineWidth: 1,
+                    lineColor: '#57D9F6',
+                    fillOpacity: '0.5',
+                    dataLabels: { enabled: false },
+                    marker: { enabled: false }
+                }
+            },
+            xAxis: {
+                type: 'datetime',
+                labels: {
+                    style: { color: 'lightgray' }
+                }
+            },
+            yAxis: {
+                gridLineColor: 'gray',
+                min: 0,
+                label: { enabled: false },
+                labels: {
+                    style: { color: 'lightgray' }
+                },
+                title: { enabled: false }
+            }
+        },
+        series: [{
+            name: 'Intake',
+            data: []
+        }, {
+            name: 'Exhaust',
+            data: []
+        }]
+    }
+
+    $scope.refresh = function(code) {
+        var resp = $scope.jmx.request($scope.req);
+        $scope.refreshLoadChart(resp[0]);
+        $scope.refreshTopicChart(resp[1], resp[2]);
+    };
+
+    $scope.refreshLoadChart = function(value) {
+        var serie = $scope.loadChart.getHighcharts().series[0];
+        serie.addPoint({x: value.timestamp * 1000, y: ((value.value * 1000) / 10)}, true, serie.data.length >= 60);
+    }
+
+    $scope.refreshTopicChart = function(intake, exhaust) {
+        var serie1 = $scope.topicChart.getHighcharts().series[0];
+        serie1.addPoint({x: intake.timestamp * 1000, y: intake.value.used}, true, serie1.data.length >= 200);
+        serie2 = $scope.topicChart.getHighcharts().series[1];
+        serie2.addPoint({x: exhaust.timestamp * 1000, y: exhaust.value.used}, true, serie2.data.length >= 200);
+    }
+
+    setInterval(function() {
+        $scope.refresh();
+    }, 2000);
 
 });
